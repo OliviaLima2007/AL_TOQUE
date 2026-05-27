b13f43131d131133743275935f8cd905e0083353
Olivia Maria, Lima Martinez <griceolivia2017@gmail.com>
2026-05-27 16:37:16 -0300

    Implement main window with options for navigation

---
+++ principal.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+
+from multas import VentanaMultas
+from turnos import VentanaTurnos
+from impuestos import VentanaImpuestos
+from expedientes import VentanaExpedientes
+
+VentanaPrincipal = tk.Tk()
+VentanaPrincipal.title("VentanaPrincipal")
+
+texto=tk.Label(VentanaPrincipal, text="Seleccione uno de los siguientes números para ingresar a:").pack()
+texto=tk.Label(VentanaPrincipal, text="1. Para ingresar a Multas").pack()
+texto=tk.Label(VentanaPrincipal, text="2. Para ingresar a Turnos").pack()
+texto=tk.Label(VentanaPrincipal, text="3. Para ingresar a Impuestos").pack()
+texto=tk.Label(VentanaPrincipal, text="4. Para ingresar a Expedientes").pack()
+
+def AbrirVentana(event):
+  match event.char:
+  case "1":
+    VentanaMultas()
+    
+  case "2":
+    VentanaTurnos()
+    
+  case "3":
+    VentanaImpuestos()
