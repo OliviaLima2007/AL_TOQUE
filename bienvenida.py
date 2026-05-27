b35b09ebf875355ee4540b3ca3ae8cae6c06bb23
Olivia Maria, Lima Martinez <griceolivia2017@gmail.com>
2026-05-27 14:14:03 -0300

    Implement user input validation in bienvenida.py
    Add input validation and instructions for user

--- bienvenida.py
+++ bienvenida.py
@@ -9,3 +9,26 @@ texto.pack()
 dni=tk.Entry(VentanaBienvenida)
 dni.config(fg="white", bg="black", font=("arial",12))
 dni.pack()
+
+texto=tk.Label(VentanaBienvenida, text="Para continuar por favor apretar ENTER")
+texto.pack()
+
+def parametros():
+  numero=int(dni.get())
+  if len(numero)!=8:
+    texto=tk.Label(VentanaBienvenida, text="Por favor ingresar 8 numeros, sin puntos ni comas")
+    texto.pack()
+    return False
+    return True
+
+def AbrirPrincipal():
+  from principal import VentanaPrincipal
+  VentanaPrincipal()
+
+def key_press(event):
+  if parametros():
+    AbrirPrincipal()
+
+VentanaPrincipal.bind("<Return>", key_press)
+
+VentanaPrincipal.mainloop()
