a43a35e36e7143c01e5285faf96c226936935955
Olivia Maria, Lima Martinez <griceolivia2017@gmail.com>
2026-06-13 14:59:36 -0300

    Refactor VentanaPrincipal to use Toplevel window

--- principal.py
+++ principal.py
@@ -1,18 +1,18 @@
 import tkinter as tk
 
 from multas import VentanaMultas
-from turnos import VentanaTurnos
+from turnos import abrir_turnos
 from impuestos import VentanaImpuestos
 from expedientes import VentanaExpedientes
 
-VentanaPrincipal = tk.Tk()
-VentanaPrincipal.title("VentanaPrincipal")
-
-texto=tk.Label(VentanaPrincipal, text="Seleccione uno de los siguientes números para ingresar a:").pack()
-texto=tk.Label(VentanaPrincipal, text="1. Para ingresar a Multas").pack()
-texto=tk.Label(VentanaPrincipal, text="2. Para ingresar a Turnos").pack()
-texto=tk.Label(VentanaPrincipal, text="3. Para ingresar a Impuestos").pack()
-texto=tk.Label(VentanaPrincipal, text="4. Para ingresar a Expedientes").pack()
+def VentanaPrincipal (ciudadanos):
+  ventana = tik.Toplevel()
+  ventana.title("VentanaPrincipal")
+  texto=tk.Label(ventana, text="Seleccione uno de los siguientes números para ingresar a:").pack()
+  texto=tk.Label(ventana, text="1. Para ingresar a Multas").pack()
+  texto=tk.Label(ventana, text="2. Para ingresar a Turnos").pack()
+  texto=tk.Label(ventana, text="3. Para ingresar a Impuestos").pack()
+  texto=tk.Label(ventana, text="4. Para ingresar a Expedientes").pack()
 
 def AbrirVentana(event):
   match event.char:
@@ -25,6 +25,5 @@ def AbrirVentana(event):
   case "4":
     VentanaExpedientes()
 
-VentanaPrincipal.bind("<KeyPress>",AbrirVentana)
-
-VentanaPrincipal.mainloop()
+ventana.bind("<KeyPress>",AbrirVentana)
+ventana.focus_set()
